9ea0a573bbddb8fefe52c775ad135432fc25f6c5
PNowakowski <p.nowakowskimusic@gmail.com>
2019-04-30 16:23:15 -0700

    Complete redux App, root, store, and root reducer

--- frontend/all_days.jsx
+++ frontend/all_days.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import { createStore } from 'redux-connect';
+import configureStore from './store/store';
+import Root from './components/root';
 
 document.addEventListener("DOMContentLoaded", () => {
     const root = document.getElementById("root");
-    // const store = createStore();
+    const store = configureStore();
 
-    ReactDOM.render(<h1>React is working!</h1>, root);
+    ReactDOM.render(<Root store={store}/>, root);
 });
---
+++ frontend/components/app.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Route, Link } from 'react-router-dom';
+
+const App = () => (
+    <div>
+        This is app!
+    </div>
+);
+
+export default App;
