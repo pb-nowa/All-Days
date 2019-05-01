17a2fc2e0d6fb557f66eb2cd6617fb9af355e975
PNowakowski <p.nowakowskimusic@gmail.com>
2019-05-01 16:53:26 -0700

    Update gitignore file and update heroku settings

--- frontend/components/account/account_container.js
+++ frontend/components/account/account_container.js
@@ -3,7 +3,6 @@ import { connect } from 'react-redux';
 import Account from './account';
 
 const mapStateToProps = state => {
-    console.log(state);
     return({
         currentUser: state.session.id,
     }); 
