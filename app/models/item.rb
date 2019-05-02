a3cc2c28aeb4f015e16481d63a9074ad95c70e7e
PNowakowski <p.nowakowskimusic@gmail.com>
2019-05-02 11:45:29 -0700

    Seed shoe data

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,15 @@
 class Item < ApplicationRecord
     has_many :cart_items
     belongs_to :shoe
+
+    # def womens_shoes
+    #     query = <<-SQL 
+    #         SELECT color DISTINCT
+    #         FROM items
+    #         WHERE gender LIKE women
+
+    #     SQL
+    #     ActiveRecord::Base.connection.execute(query)
+    # end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,13 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 demo = User.create!(first_name: "demo", last_name: "user", email: "demouser@demouser.com", password: "demouser")
+
+shoe_1 = Shoe.create!(style: "Runner", name: "Men's Wool Runners", material: "Wool", gender: "mens", price: 95)
+shoe_2 = Shoe.create!(style: "Lounger", name: "Men's Wool Loungers", material: "Wool", gender: "mens", price: 95)
+shoe_3 = Shoe.create!(style: "Topper", name: "Men's Tree Toppers", material: "Tree", gender: "mens", price: 155)
+shoe_4 = Shoe.create!(style: "Skipper", name: "Men's Tree Skippers", material: "Tree", gender: "mens", price: 95)
+
+shoe_5 = Shoe.create!(style: "Runner", name: "Women's Wool Runners", material: "Wool", gender: "womens", price: 95)
+shoe_6 = Shoe.create!(style: "Topper", name: "Women's Tree Toppers", material: "Tree", gender: "womens", price: 115)
+shoe_7 = Shoe.create!(style: "Breezer", name: "Women's Tree Breezers", material: "Tree", gender: "womens", price: 95)
+shoe_8 = Shoe.create!(style: "Skipper", name: "Women's Tree Skippers", material: "Tree", gender: "womens", price: 95)
