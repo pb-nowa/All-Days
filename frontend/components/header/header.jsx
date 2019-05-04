3f17324a64bcbe6ab46bd33ac11c83018274b52b
PNowakowski <p.nowakowskimusic@gmail.com>
2019-05-04 15:30:02 -0700

    In the middle of css-ing the dropdown menu

--- frontend/components/header/header.jsx
+++ frontend/components/header/header.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link, Route } from 'react-router-dom';
+import ShoesDropdown from './header_shoes_dropdown';
 
 class Header extends React.Component {
     constructor(props){
@@ -25,17 +26,25 @@ class Header extends React.Component {
 render() {
             return (
             <div className={this.state.scrollPosition ? "active-header" : "header"} id="header">
-                <div  className="nav" id="left-nav">
-                    <div className="nav-link"><Link to={'/collections/mens'}><h2>MEN</h2></Link></div>
-                    <div className="nav-link"><Link to={'/collections/womens'}><h2>WOMEN</h2></Link></div>
-                    <div className="nav-link"><h2>ABOUT</h2></div>
+                <div id="navs">
+                    <div  className="nav" id="left-nav">
+                        <div className="nav-link">
+                            <h2>MEN</h2>
+                        </div>
+                        <div className="nav-link"><Link to={'/collections/womens'}><h2>WOMEN</h2></Link></div>
+                        <div className="nav-link"><h2>ABOUT</h2></div>
+                    </div>
+                    <div id="logo">
+                        <Link to="/"><h1>Alldays</h1></Link>
+                    </div>
+                    <div className="nav" id="right-nav">
+                        <div className="nav-link"><Link to={'/account'}><h2 id="account-link">ACCOUNT</h2></Link></div>
+                        <div className="nav-link"><h2>CART</h2></div>
+                    </div>
                 </div>
-                <div id="logo">
-                    <Link to="/"><h1>Alldays</h1></Link>
-                </div>
-                <div className="nav" id="right-nav">
-                    <div className="nav-link"><Link to={'/account'}><h2 id="account-link">ACCOUNT</h2></Link></div>
-                    <div className="nav-link"><h2>CART</h2></div>
+
+                <div id="nav-dropdown">
+                    <ShoesDropdown gender={'Men'} />
                 </div>
             </div>
         )
--- frontend/components/header/header_shoes_dropdown.jsx
+++ frontend/components/header/header_shoes_dropdown.jsx
@@ -1,17 +1,56 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 class ShoesDropdown extends React.Component {
     constructor(props){
         super(props);
-        
+        this.state = {
+            gender: this.props.gender,
+        };
     }
 
     render() {
         
         return (
             <div>
-
+                <ul>
+                    <li>
+                        <div className="dropdown-shoe-image">missing image</div>
+                        <h3>this.state.shoe</h3>
+                    </li>
+                    <li>
+                        <div className="dropdown-shoe-image">missing image</div>
+                        <h3>this.state.shoe</h3>
+                    </li>
+                    <li>
+                        <div className="dropdown-shoe-image">missing image</div>
+                        <h3>this.state.shoe</h3>
+                    </li>
+                    <li>
+                        <div className="dropdown-shoe-image">missing image</div>
+                        <h3>this.state.shoe</h3>
+                    </li>
+                </ul>
+                <div>
+                    <h2>Shop All {this.state.gender}</h2>
+                    <div>
+                        <ul>
+                            <li>
+                                <h3>BY STYLE</h3>
+                                <ul>
+                                    <Link to={`/collections/${this.state.gender}`}>Runners</Link>
+                                    <Link to={`/collections/${this.state.gender}`}>Toppers</Link>
+                                    <Link to={`/collections/${this.state.gender}`}>Loungers</Link>
+                                    <Link to={`/collections/${this.state.gender}`}>Skippers</Link>
+                                </ul>
+                            </li>
+                            <li>BY COLOR</li>
+                        </ul>
+                    </div>
+                </div>
             </div>
         )
     }
 }
+
+export default ShoesDropdown;
