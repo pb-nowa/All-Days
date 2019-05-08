a0be8541752fdb17d10378ab2378dfbf0f1b491f
PNowakowski <p.nowakowskimusic@gmail.com>
2019-05-08 10:06:19 -0700

    Fix header animations and color on '/account'

--- frontend/components/header/header.jsx
+++ frontend/components/header/header.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link, Route } from 'react-router-dom';
 import ShoesDropdown from './header_shoes_dropdown';
+import { withRouter } from 'react-router-dom';
 
 class Header extends React.Component {
     constructor(props){
@@ -23,19 +24,21 @@ class Header extends React.Component {
         window.addEventListener('scroll', this.handleScroll);
     }
 
+    
+
     handleScroll() {
         this.setState( (state) => ({ scrollPosition: window.pageYOffset }) );
-        this.setState( state => ({ dropDownIsActive: !!(state.scrollPosition || state.isHovered || state.dropDown) }) );
+        this.setState( state => ({ dropDownIsActive: !!(state.scrollPosition || state.isHovered || state.dropDown || this.props.location.pathname === "/account") }) );
     }
 
     enterHeaderHover(){
         this.setState( () => ({ isHovered: true }) );
-        this.setState( state => ({ dropDownIsActive: !!(state.scrollPosition || state.isHovered || state.dropDown) }) );
+        this.setState(state => ({ dropDownIsActive: !!(state.scrollPosition || state.isHovered || state.dropDown || this.props.location.pathname === "/account") }) );
     }
 
     leaveHeaderHover(){
         this.setState( () => ({ isHovered: false }) );
-        this.setState( state => ({ dropDownIsActive: !!(state.scrollPosition || state.isHovered || state.dropDown) }) );
+    this.setState(state => ({ dropDownIsActive: !!(state.scrollPosition || state.isHovered || state.dropDown || this.props.location.pathname === "/account") }) );
     }
 
     handleDropDown(dropDownGender){
@@ -53,29 +56,29 @@ class Header extends React.Component {
     }
 
     render() {
-        console.log(this.state);
+        const dropDownIsActive = this.state.dropDownIsActive;
         return (
         <>
         <div onMouseEnter={this.enterHeaderHover} 
             onMouseLeave={this.leaveHeaderHover}
-            className={ this.state.dropDownIsActive ? "active-header" : "header"} 
+            className={ dropDownIsActive ? "active-header" : "header"} 
             id="header">
             <div id="navs">
                 <div  className="nav" id="left-nav">
-                        <div className={(this.state.scrollPosition || this.state.isHovered || this.state.dropDown ? "a-header-button" : "u-header-button") + " nav-link"}>
+                    <div className={(dropDownIsActive ? "a-header-button" : "u-header-button") + " nav-link"}>
                         <h2 onClick={() => this.handleDropDown("Men")}>MEN</h2>
                     </div>
-                        <div className={(this.state.scrollPosition || this.state.isHovered || this.state.dropDown ? "a-header-button" : "u-header-button") + " nav-link" }>
+                    <div className={(dropDownIsActive ? "a-header-button" : "u-header-button") + " nav-link" }>
                         <h2 onClick={() => this.handleDropDown("Women")}>WOMEN</h2>
                     </div>
-                        <div className={(this.state.scrollPosition || this.state.isHovered || this.state.dropDown ? "a-header-button" : "u-header-button") + " nav-link" }><h2>ABOUT</h2></div>
+                    <div className={(dropDownIsActive || this.state.isHovered || this.state.dropDown ? "a-header-button" : "u-header-button") + " nav-link" }><h2>ABOUT</h2></div>
                 </div>
                 <div id="logo">
-                    <Link to="/" className={(this.state.scrollPosition || this.state.isHovered || this.state.dropDown ? "a-header-button" : "u-header-button") + " nav-link logo"}><h1>Alldays</h1></Link>
+                    <Link to="/" className={(dropDownIsActive ? "a-header-button" : "u-header-button") + " nav-link logo"}><h1>Alldays</h1></Link>
                 </div>
                 <div className="nav" id="right-nav">
-                    <div ><Link to={'/account'}><h2 className={(this.state.scrollPosition || this.state.isHovered || this.state.dropDown ? "a-header-button" : "u-header-button") + " nav-link"} id="account-link">ACCOUNT</h2></Link></div>
-                    <div className={(this.state.scrollPosition || this.state.isHovered || this.state.dropDown ? "a-header-button" : "u-header-button") + " nav-link"}><h2>CART</h2></div>
+                    <div ><Link to={'/account'}><h2 className={(dropDownIsActive ? "a-header-button" : "u-header-button") + " nav-link"} id="account-link">ACCOUNT</h2></Link></div>
+                    <div className={(dropDownIsActive ? "a-header-button" : "u-header-button") + " nav-link"}><h2>CART</h2></div>
                 </div>
             </div>
         </div>
@@ -88,4 +91,4 @@ class Header extends React.Component {
 
 }
 
-export default Header;
+export default withRouter(Header);
