4226c9856a79299f896b79d7763ca5bc6b8115bd
PNowakowski <p.nowakowskimusic@gmail.com>
2019-06-26 10:36:15 -0700

    Create cart sidebar, position fixed

--- frontend/components/cart/cart.jsx
+++ frontend/components/cart/cart.jsx
@@ -9,34 +9,39 @@ class Cart extends React.Component {
 
     render(){
         return(
-            <div className="cart">
-                <div>
-                    <h2>CART</h2>
-                    <div>Click to Close</div>
-                </div>
-                <ul>
-                    <li>cart item component</li>
-                    <li>cart item component</li>
-                    <li>cart item component</li>
-                </ul>
-                <div>Looking for something else?</div>
-                <Link to={'/collections/mens'}>Keep Shopping</Link>
-                <div>
+        <div className="cart-page-container">
+            <div className="cart-container">
+                <div className="overlay-visible"></div>
+                <div className="cart-sidebar">
+                    <div className="cart-header">
+                        <h2>CART</h2>
+                        <div>close</div>
+                    </div>
+                    <ul>
+                        <li>cart item component</li>
+                        <li>cart item component</li>
+                        <li>cart item component</li>
+                    </ul>
+                    <div>Looking for something else?</div>
+                    <Link to={'/collections/mens'}>Keep Shopping</Link>
                     <div>
-                        <h3>Subtotal</h3>
-                        <div>Cost var</div>
+                        <div>
+                            <h3>Subtotal</h3>
+                            <div>Cost var</div>
+                        </div>
+                        <div>
+                            <h3>Shipping</h3>
+                            <div>FREE</div>
+                        </div>
+                        <input type="button" value="CHECKOUT"/>
                     </div>
                     <div>
-                        <h3>Shipping</h3>
-                        <div>FREE</div>
+                        <p>Looking for more shoes?</p>
+                        <p>Click <Link to={'colelctions/mens'}>here</Link></p>
                     </div>
-                    <input type="button" value="CHECKOUT"/>
-                </div>
-                <div>
-                    <p>Looking for more shoes?</p>
-                    <p>Click <Link to={'colelctions/mens'}>here</Link></p>
                 </div>
             </div>
+        </div>
         )
     }
 }
